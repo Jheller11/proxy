f471cc5308896c27f635b858e11e30ac1882f4f5
Jeffrey Heller <heller.jeffrey@gmail.com>
2019-05-16 14:04:42 -0400

    Install/Config Express and related packages

---
+++ index.js
@@ -0,0 +1,32 @@
+// packages
+const express = require('express')
+const app = express()
+const override = require('method-override')
+const bodyParser = require('body-parser')
+const helmet = require('helmet')
+const compression = require('compression')
+
+// config
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(override('_method'))
+app.use(compression())
+app.use(helmet())
+
+// controllers
+
+// home route
+app.get('/', (req, res, next) => {
+  res.send('Welcome to my server.').status(200)
+})
+
+// error handler
+app.use((error, req, res, next) => {
+  if (error) res.send(error)
+  next()
+})
+// set port
+app.set('port', process.env.PORT || 4000)
+
+app.listen(app.get('port'), () =>
+  console.log('server running on ' + app.get('port'))
+)
