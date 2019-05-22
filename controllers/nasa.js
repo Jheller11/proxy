899ce05a333314f46638076f8cd82633c87c2d4f
Jeffrey Heller <heller.jeffrey@gmail.com>
2019-05-22 16:52:21 -0400

    config nasa image/video requests

--- controllers/nasa.js
+++ controllers/nasa.js
@@ -5,6 +5,7 @@ const processNewRequest = require('../utils').processNewRequest
 // github repo: https://github.com/Jheller11/nasa-images
 // deployed application: https://nasa-image-search.surge.sh
 
+// astronomy picture of the day
 router.get('/apod', (req, res, next) => {
   let url = `https://api.nasa.gov/planetary/apod?api_key=${
     process.env.NASA_APP_NASA_API_KEY
@@ -12,14 +13,22 @@ router.get('/apod', (req, res, next) => {
   processNewRequest(url, res, next)
 })
 
+// natural epic photo array (current date)
 router.get('/epic/natural', (req, res, next) => {
   let url = 'https://epic.gsfc.nasa.gov/api/'
   processNewRequest(url + 'natural', res, next)
 })
 
+// enhanced epic photo array (current date)
 router.get('/epic/enhanced', (req, res, next) => {
   let url = 'https://epic.gsfc.nasa.gov/api/'
   processNewRequest(url + 'enhanced', res, next)
+})
+
+// image/video search
+router.post('/search', async (req, res, next) => {
+  let url = req.body.url
+  processNewRequest(url, res, next)
 })
 
 module.exports = router
