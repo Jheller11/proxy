70328ecfe76175834902c20d023bf7819c152b7f
Jeffrey Heller <heller.jeffrey@gmail.com>
2019-05-22 10:59:43 -0400

    route and config nasa/apod

--- controllers/nasa.js
+++ controllers/nasa.js
@@ -1,7 +1,11 @@
 const router = require('express').Router()
+const processNewRequest = require('../utils').processNewRequest
 
-router.get('/', (req, res, next) => {
-  res.send('hello nasa controller')
+router.get('/apod', (req, res, next) => {
+  let url = `https://api.nasa.gov/planetary/apod?api_key=${
+    process.env.NASA_APP_NASA_API_KEY
+  }`
+  processNewRequest(url, res, next)
 })
 
 module.exports = router
