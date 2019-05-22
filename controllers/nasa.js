b32c00bdb1bcc8102cadd2595f1fc1d2dd0cf2c2
Jeffrey Heller <heller.jeffrey@gmail.com>
2019-05-22 11:49:08 -0400

    proxy nasa/epic requests through server

--- controllers/nasa.js
+++ controllers/nasa.js
@@ -1,11 +1,25 @@
 const router = require('express').Router()
 const processNewRequest = require('../utils').processNewRequest
 
+// proxy for requests from nasa application
+// github repo: https://github.com/Jheller11/nasa-images
+// deployed application: https://nasa-image-search.surge.sh
+
 router.get('/apod', (req, res, next) => {
   let url = `https://api.nasa.gov/planetary/apod?api_key=${
     process.env.NASA_APP_NASA_API_KEY
   }`
   processNewRequest(url, res, next)
+})
+
+router.get('/epic/natural', (req, res, next) => {
+  let url = 'https://epic.gsfc.nasa.gov/api/'
+  processNewRequest(url + 'natural', res, next)
+})
+
+router.get('/epic/enhanced', (req, res, next) => {
+  let url = 'https://epic.gsfc.nasa.gov/api/'
+  processNewRequest(url + 'enhanced', res, next)
 })
 
 module.exports = router
